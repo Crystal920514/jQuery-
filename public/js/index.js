1058105d25e0fc5295080c9daa87d64b921c568e
Crystal zj <zjcapf1234@163.com>
2018-03-21 16:34:13 +0800

    2.0

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+$(function () {
+  $('.top-list>li').hover(function () {
+    $(this).find('.top-list-hook').show();
+  },function () {
+    $(this).find('.top-list-hook').hide();
+  })
+});
+
+$(function () {
+  $(document).scroll(function() {
+    var top = $(document).scrollTop()
+    console.log(top);
+    if(top>70){
+      $('#header').addClass('on')
+      console.log(true);
+    }else {
+      $('#header').removeClass('on')
+      console.log(false);
+    }
+  })
+})
